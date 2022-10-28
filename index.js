152725ddd64055e65e443636d2184eef0f13bbca
Mike Tagg <mdtagg91@gmail.com>
2022-10-28 13:14:31 -0700

    turned create boxes into an IIFE

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 
-const boxes = document.querySelector('.container')
 
-function createBoxes() {
+(function createBoxes() {
+    const boxes = document.querySelector('.container')
     for(let i = 1;i < 10;i++) {
         let box = document.createElement('div')
         box.setAttribute('data-attribute',`${i}`)
-        if(i < 3) {
+        if(i < 3 || i >= 4 && i < 6) {
             box.classList.add('cornerBorder')
         }
         else if (i === 3 || i === 6) {
             box.classList.add('bottomBorder')
         }
-        else if (i < 6) {
-            box.classList.add('cornerBorder')
-        }else if (i < 9) {
+        else if (i < 9) {
             box.classList.add('borderRight')
         }
         boxes.appendChild(box)
     }
-}
+})()
 
-
-createBoxes()
-
-// for(let i = 1;i < 10;i++) {
-//     console.log(i)
-//     let box = document.createElement('div')
-//     if(i % 3 === 0 && i !== 9) {
-//         box.style.borderBottom = '1px solid black'
-//     }else if(i > 6 && i !== 9) {
-//         box.style.borderRight = '1px solid black'
-//     }else {
-//         box.classList.add('blackBorder')
-//     }
-// }
